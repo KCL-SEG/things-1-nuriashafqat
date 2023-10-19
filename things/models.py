ea58a7f14538f5b33fbc70888e17ed190dff8a76
nuriashafqat <nuriashafqat02@gmail.com>
2023-10-19 13:13:59 +0100

    Fix spelling mistake

--- things/models.py
+++ things/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from djando.db import Model
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
+
 
 class Thing(models.Model):
     name = models.CharField(max_length=30,blank=False, unique = True)
