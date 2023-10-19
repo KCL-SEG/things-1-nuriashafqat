684693c36e743af0a768bccf0bd8e2290b3ec876
nuriashafqat <nuriashafqat02@gmail.com>
2023-10-19 10:06:05 +0100

    Add validator for integer field, make sure boundaries are correct for attributes

--- things/models.py
+++ things/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(max_length=500)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30,blank=False, unique = True)
+    description = models.TextField(max_length=120, blank=True, unique = False)
+    quantity = models.IntegerField(
+        validators=[MaxValueValidator(100)]
+    )
 
 
