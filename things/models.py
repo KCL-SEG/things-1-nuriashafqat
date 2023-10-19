df91ef50f5d173d4d8880b6896029494a6445096
nuriashafqat <nuriashafqat02@gmail.com>
2023-10-19 10:09:56 +0100

    Specify range for IntegerField

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ class Thing(models.Model):
     name = models.CharField(max_length=30,blank=False, unique = True)
     description = models.TextField(max_length=120, blank=True, unique = False)
     quantity = models.IntegerField(
-        validators=[MaxValueValidator(100)]
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
 
 
