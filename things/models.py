b18456a54edb36d4e05cf2fb1ad685941193de9d
nuriashafqat <nuriashafqat02@gmail.com>
2023-10-19 13:33:56 +0100

    Change type of description field to Charfield

--- things/models.py
+++ things/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30,blank=False, unique = True)
-    description = models.TextField(max_length=120, blank=True, unique = False)
+    description = models.CharField(max_length=120, blank=True, unique = False)
     quantity = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
